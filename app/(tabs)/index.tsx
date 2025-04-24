a72176b788509e99e4605d815863a6ba51bec589
iamMAHAM <abdulkabore@gmail.com>
2025-04-24 17:03:11 +0000

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,63 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  PlatformPay,
+  PlatformPayButton,
+  StripeProvider,
+  usePlatformPay,
+} from '@stripe/stripe-react-native';
+import { useEffect, useState } from 'react';
+import { Alert, Text, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const fetchKey = () => Promise.resolve('publishable_key');
 
-export default function HomeScreen() {
+function App() {
+  const [publishableKey, setPublishableKey] = useState('');
+  const [isSupported, setIsSupported] = useState(false);
+
+  const fetchPublishableKey = async () => {
+    const key = await fetchKey(); // fetch key from your server here
+    setPublishableKey(key);
+  };
+
+  const { isPlatformPaySupported } = usePlatformPay();
+
+  useEffect(() => {
+    (async function () {
+      const isAvailable = await isPlatformPaySupported({
+        googlePay: { testEnv: true },
+      }); // always false in android
+      setIsSupported(isAvailable);
+    })();
+  }, []);
+
+  useEffect(() => {
+    fetchPublishableKey();
+  }, []);
+
+  const pay = async () => {
+    console.log('pay');
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <StripeProvider
+      publishableKey={publishableKey}
+      merchantIdentifier='merchant.identifier' // required for Apple Pay
+      urlScheme='your-url-scheme' // required for 3D Secure and bank redirects
+    >
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        {isSupported ? (
+          <PlatformPayButton
+            type={PlatformPay.ButtonType.Pay}
+            onPress={pay}
+            style={{
+              width: '100%',
+              height: 50,
+            }}
+          />
+        ) : (
+          <Text style={{ fontSize: 22 }}>Plateform pay is not supported</Text>
+        )}
+      </View>
+    </StripeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default App;
